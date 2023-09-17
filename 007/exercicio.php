867bbadaa952d90481328c7f4df0f11b4ff7a967
Alan Patrick <alan.patrick.fv@gmail.com>
2023-09-17 18:10:19 -0300

    Criar exercício 007

---
+++ 007/exercicio.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Exercício 007.1: Tipos de números
+ * 
+ * Altere os seguintes ecos, para que mostre 
+ * qual é o tipo de número.
+ * 
+ * Em php, existem dois tipos de números: 
+ * Inteiros(integers) e Decimais(floats)
+ * 
+ * Use a função var_dump()
+ */
+
+ echo 2;  // Deve aparecer int(2)
+ echo 3.14; // Deve aparecer float(3.14)
+
+ // A função var_dump pode ser usada para descobrir o tipo de qualquer valor\variável.
+
+ echo "<br>";// fim do exercício
+
+/**
+ * Exercício 007.2 
+ * 
+ */
+
+?>
+
+<p>
+  <a href="https://www.w3schools.com/php/php_numbers.asp">Referência</a>
+</p>
