9152607442879931c6f9ae7d97ecd643e9cc3d9f
Alan Patrick <alan.patrick.fv@gmail.com>
2023-09-18 22:19:15 -0300

    Atualizar versão

---
+++ 010/solucao.php
@@ -0,0 +1,15 @@
+<iframe src="./Tabela de operadores.html" width="800px" height="500px" frameborder="0"></iframe>
+<?php
+/**
+ * 
+ */
+echo "<br><br>";
+
+ $a = "ola";
+ $b = "mundo";
+ $c = $a . ' ' . $b;
+
+ echo $a . ' ' . $b;
+ echo "<br>";
+ echo $c;
+?>
